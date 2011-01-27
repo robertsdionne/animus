ff05a65b3031797a231e1ea72a76a2adbb870594
Robert Dionne <robertsdionne@gmail.com>
2011-01-26 23:17:58 -0500

    Integrating with Google Closure. Adding genjsdeps.sh. Making render loop bare-bones.

---
+++ genjsdeps.sh
@@ -0,0 +1,6 @@
+# Copyright 2011 Robert Scott Dionne. All Rights Reserved.
+#
+# Generates Javascript dependencies.
+../closure-library/closure/bin/build/depswriter.py \
+  --root_with_prefix=". ../../../animus/" \
+  --output_file=deps.js
