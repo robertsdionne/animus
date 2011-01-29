929b6f72ffbdac0de7e5e009b930a297b711bc30
Robert Dionne <robertsdionne@gmail.com>
2011-01-29 18:21:53 -0500

    Restructured program.js to use concise prototype syntax.

--- program.js
+++ program.js
@@ -16,32 +16,30 @@ webgl.Program = function(vertex, fragment) {
 };
 
 
-webgl.Program.prototype.create = function(gl) {
-  this.vertex_.create(gl);
-  this.fragment_.create(gl);
-  this.handle = gl.createProgram();
-};
-
-
-webgl.Program.prototype.link = function(gl) {
-  this.vertex_.compile(gl);
-  gl.attachShader(this.handle, this.vertex_.handle);
-  this.fragment_.compile(gl);
-  gl.attachShader(this.handle, this.fragment_.handle);
-  gl.linkProgram(this.handle);
-  if (!gl.getProgramParameter(this.handle, gl.LINK_STATUS)) {
-    var log = gl.getShaderInfoLog(this.handle);
-    this.dispose();
-    throw new Error(log);
+webgl.Program.prototype = {
+  create = function(gl) {
+    this.vertex_.create(gl);
+    this.fragment_.create(gl);
+    this.handle = gl.createProgram();
+  },
+  dispose: function(gl) {
+    gl.detachShader(this.handle, this.vertex_.handle);
+    this.vertex_.dispose(gl);
+    gl.detachShader(this.handle, this.fragment_.handle);
+    this.fragment_.dispose(gl);
+    gl.deleteProgram(this.handle);
+    this.handle = null;
+  },
+  link: function(gl) {
+    this.vertex_.compile(gl);
+    gl.attachShader(this.handle, this.vertex_.handle);
+    this.fragment_.compile(gl);
+    gl.attachShader(this.handle, this.fragment_.handle);
+    gl.linkProgram(this.handle);
+    if (!gl.getProgramParameter(this.handle, gl.LINK_STATUS)) {
+      var log = gl.getShaderInfoLog(this.handle);
+      this.dispose();
+      throw new Error(log);
+    }
   }
-};
-
-
-webgl.Program.prototype.dispose = function(gl) {
-  gl.detachShader(this.handle, this.vertex_.handle);
-  this.vertex_.dispose(gl);
-  gl.detachShader(this.handle, this.fragment_.handle);
-  this.fragment_.dispose(gl);
-  gl.deleteProgram(this.handle);
-  this.handle = null;
 };
