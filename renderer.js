c0239f6d3fd9d2479834563c6e703a1796200b33
Robert Dionne <robertsdionne@gmail.com>
2011-01-29 18:23:56 -0500

    Restructured renderer.js to use concise prototype syntax.

--- renderer.js
+++ renderer.js
@@ -15,31 +15,33 @@ goog.provide('webgl.Renderer');
 webgl.Renderer = function() {};
 
 
-/**
- * Handles changes in WebGL canvas size.
- * @param {WebGLRenderingContext} gl The WebGL rendering context.
- * @param {number} width The new canvas width.
- * @param {number} height Thew new canvas height.
- */
-webgl.Renderer.prototype.onChange = goog.abstractMethod;
+webgl.Renderer.prototype = {
+  /**
+   * Handles changes in WebGL canvas size.
+   * @param {WebGLRenderingContext} gl The WebGL rendering context.
+   * @param {number} width The new canvas width.
+   * @param {number} height Thew new canvas height.
+   */
+  onChange: goog.abstractMethod,
 
 
-/**
- * Handles WebGL context creation.
- * @param {WebGLRenderingContext} gl The WebGL rendering context.
- */
-webgl.Renderer.prototype.onCreate = goog.abstractMethod;
+  /**
+   * Handles WebGL context creation.
+   * @param {WebGLRenderingContext} gl The WebGL rendering context.
+   */
+  onCreate: goog.abstractMethod,
 
 
-/**
- * Handles WebGL context destruction.
- * @param {WebGLRenderingContext} gl The WebGL rendering context.
- */
-webgl.Renderer.prototype.onDestroy = goog.abstractMethod;
+  /**
+   * Handles WebGL context destruction.
+   * @param {WebGLRenderingContext} gl The WebGL rendering context.
+   */
+  onDestroy: goog.abstractMethod,
 
 
-/**
- * Handles WebGL drawing.
- * @param {WebGLRenderingContext} gl The WebGL rendering context.
- */
-webgl.Renderer.prototype.onDraw = goog.abstractMethod;
+  /**
+   * Handles WebGL drawing.
+   * @param {WebGLRenderingContext} gl The WebGL rendering context.
+   */
+  onDraw: goog.abstractMethod
+};
