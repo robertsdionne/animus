2066a23385927cb42f0e8d231d971071ef4d7820
Robert Dionne <robertsdionne@gmail.com>
2011-01-29 18:32:09 -0500

    Adding Asset.

---
+++ asset.js
@@ -0,0 +1,23 @@
+// Copyright 2011 Robert Scott Dionne. All Rights Reserved.
+
+/**
+ * @fileoverview
+ * @author robertsdionne@gmail.com (Robert Scott Dionne)
+ */
+
+goog.provide('animus.Asset');
+
+
+/**
+ * @constructor
+ */
+animus.Asset = function(id) {
+  this.id_ = id;
+};
+
+
+animus.Asset.prototype = {
+  id: function() {
+    return this.id_;
+  }
+};
