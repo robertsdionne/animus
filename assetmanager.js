2ee097b476627a55b97a623d3c1e96e5dd2af9cc
Robert Dionne <robertsdionne@gmail.com>
2011-01-29 18:52:04 -0500

    Implementing a rough sketch of an AssetManager. Still need AssetLoader(s).

--- assetmanager.js
+++ assetmanager.js
@@ -4,3 +4,53 @@
  * @fileoverview
  * @author robertsdionne@gmail.com (Robert Scott Dionne)
  */
+
+goog.provide('animus.AssetManager');
+
+goog.require('goog.structs.Map');
+
+
+animus.AssetManager = function() {
+  this.nextId_ = 0;
+  this.assetMap_ = new goog.structs.Map();
+};
+goog.addSingletonGetter(animus.AssetManager);
+
+
+animus.AssetManager.prototype = {
+  /**
+   * Gets an Asset from the AssetManager.
+   * @param {number} id
+   * @return {Asset} The requested Asset, or undefined.
+   */
+  getAsset: function(id) {
+    return this.assetMap_.get(id);
+  },
+
+
+  /**
+   * Puts an Asset into the AssetManager.
+   * @param {number} id
+   * @param {animus.Asset} asset
+   * @return {boolean} True if the Asset was added to the AssetManager,
+   *     false otherwise.
+   */
+  putAsset: function(id, asset) {
+    if (this.assetMap_.containsKey(id)) {
+      return false;
+    }
+    if (!(asset instanceof Asset)) {
+      return false;
+    }
+    this.assetMap_.set(id, asset);
+  },
+
+
+  /**
+   * Reserves an id for a new Asset.
+   * @return {number} The id reserved for the new Asset.
+   */
+  reserveId: function() {
+    return this.nextId_++;
+  }
+};
