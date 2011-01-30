ea41fb684a8fdbdd9136a41e59f4bf55b1e033a9
Robert Dionne <robertsdionne@gmail.com>
2011-01-29 19:11:51 -0500

    Adding AssetLoader.

---
+++ assetloader.js
@@ -0,0 +1,17 @@
+// Copyright 2011 Robert Scott Dionne. All Rights Reserved.
+
+/**
+ * @fileoverview
+ * @author robertsdionne@gmail.com (Robert Scott Dionne)
+ */
+
+goog.provide('animus.AssetLoader');
+
+
+animus.AssetLoader = function() {
+};
+goog.addSingletonGetter(animus.AssetLoader);
+
+
+animus.AssetLoader.prototype = {
+};
