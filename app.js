18ebfb15a2b6652dcfc52ce8db9478b7dec412ea
Robert Dionne <robertsdionne@gmail.com>
2011-01-29 20:56:03 -0500

    Reverting app.js to verbose prototype syntax.

--- app.js
+++ app.js
@@ -34,59 +34,57 @@ webgl.App.WEBGL_CONTEXT = 'experimental-webgl';
 webgl.App.INTERVAL_MS = 10;
 
 
-webgl.App.prototype = {
-  /**
-   * Checks that the dimensions and, if so, dispatches onChange
-   * to the Renderer.
-   * @private
-   */
-  checkDimensions_: function() {
-    if (this.width_ !== this.canvas_.width ||
-        this.height_ !== this.canvas_.height) {
-      this.width_ = this.canvas_.width;
-      this.height_ = this.canvas_.height;
-      this.renderer_.onChange(this.gl_, this.width_, this.height_);
-    }
-  },
-
-
-  /**
-   * Associates this App with the given canvas
-   * and starts the rendering loop.
-   * @param {Element} canvas The canvas.
-   */
-  install: function(canvas) {
-    this.canvas_ = canvas;
-    this.gl_ = this.canvas_.getContext(webgl.App.WEBGL_CONTEXT);
-    this.renderer_.onCreate(this.gl_);
-    this.intervalHandle_ = this.window_.setInterval(
-        goog.bind(this.onFrame_, this),
-        webgl.App.INTERVAL_MS);
-  },
-
-
-  /**
-   * Dispatches onChange and onDraw events to the Renderer.
-   * @private
-   */
-  onFrame_: function() {
-    this.checkDimensions_();
-    this.renderer_.onDraw(this.gl_);
-  },
-
-
-  /**
-   * Dissociates this App with the previously associated canvas
-   * and stops the rendering loop.
-   */
-  uninstall: function() {
-    if (this.intervalHandle_) {
-      this.window_.clearInterval(this.intervalHandle_);
-    }
-    this.renderer_.onDestroy(this.gl_);
-    this.width_ = this.height_ = 0;
-    this.canvas_ = null;
-    this.gl_ = null;
-    this.intervalHandle_ = null;
+/**
+ * Checks that the dimensions and, if so, dispatches onChange
+ * to the Renderer.
+ * @private
+ */
+webgl.App.prototype.checkDimensions_ = function() {
+  if (this.width_ !== this.canvas_.width ||
+      this.height_ !== this.canvas_.height) {
+    this.width_ = this.canvas_.width;
+    this.height_ = this.canvas_.height;
+    this.renderer_.onChange(this.gl_, this.width_, this.height_);
   }
+};
+
+
+/**
+ * Associates this App with the given canvas
+ * and starts the rendering loop.
+ * @param {Element} canvas The canvas.
+ */
+webgl.App.prototype.install = function(canvas) {
+  this.canvas_ = canvas;
+  this.gl_ = this.canvas_.getContext(webgl.App.WEBGL_CONTEXT);
+  this.renderer_.onCreate(this.gl_);
+  this.intervalHandle_ = this.window_.setInterval(
+      goog.bind(this.onFrame_, this),
+      webgl.App.INTERVAL_MS);
+};
+
+
+/**
+ * Dispatches onChange and onDraw events to the Renderer.
+ * @private
+ */
+webgl.App.prototype.onFrame_ = function() {
+  this.checkDimensions_();
+  this.renderer_.onDraw(this.gl_);
+};
+
+
+/**
+ * Dissociates this App with the previously associated canvas
+ * and stops the rendering loop.
+ */
+webgl.App.prototype.uninstall = function() {
+  if (this.intervalHandle_) {
+    this.window_.clearInterval(this.intervalHandle_);
+  }
+  this.renderer_.onDestroy(this.gl_);
+  this.width_ = this.height_ = 0;
+  this.canvas_ = null;
+  this.gl_ = null;
+  this.intervalHandle_ = null;
 };
