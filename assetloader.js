f0794159ac0f6ae5f3803bda153a556a7670524f
Robert Dionne <robertsdionne@gmail.com>
2011-01-29 21:09:22 -0500

    Implemented AssetLoader for Character.

--- assetloader.js
+++ assetloader.js
@@ -7,11 +7,64 @@
 
 goog.provide('animus.AssetLoader');
 
+goog.require('animus.AssetManager');
+goog.require('animus.Character');
+
+goog.require('goog.structs.Map');
+
 
 animus.AssetLoader = function() {
 };
 goog.addSingletonGetter(animus.AssetLoader);
 
 
-animus.AssetLoader.prototype = {
+animus.AssetLoader.prototype.loadAssets = function(json) {
+  var result = JSON.parse(json);
+  var idMap = new goog.structs.Map();
+  for (var i = 0; i < result.character.length; ++i) {
+    this.loadAsset_(animus.Character, result.character[i], idMap);
+  }
+};
+
+
+animus.AssetLoader.prototype.loadAsset_ = function(assetCtor, object, idMap) {
+  var translated =
+      this.translateIdsInObject_(object, assetCtor.LOAD_MASK, idMap);
+  var asset = new assetCtor(translated);
+  animus.AssetManager.getInstance().putAsset(asset);
+};
+
+
+animus.AssetLoader.prototype.translateIdsInObject_ =
+    function(object, mask, idMap) {
+  var translated = {};
+  for (var property in mask) {
+    if (object[property].length) {
+      translated[property] =
+          this.translateIdsInArray_(object[property], idMap);
+    } else {
+      translated[property] = this.translateId_(object[property], idMap);
+    }
+  }
+  return translated;
+};
+
+
+animus.AssetLoader.prototype.translateIdsInArray_ = function(array, idMap) {
+  var translated = [];
+  for (var i = 0; i < array.length; ++i) {
+    translated[i] = this.translateId_(array[i], idMap);
+  }
+  return translated;
+};
+
+
+animus.AssetLoader.prototype.translateId_ = function(id, idMap) {
+  if (idMap.containsKey(id)) {
+    return idMap.get(id);
+  } else {
+    var newId = animus.AssetManager.getInstance().reserveId();
+    idMap.set(id, newId);
+    return newId;
+  }
 };
--- deps.js
+++ deps.js
@@ -3,9 +3,9 @@
 goog.addDependency('../../../animus/animus.js', ['animus'], ['goog.dom', 'webgl.App', 'webgl.Program', 'webgl.Renderer', 'webgl.Shader']);
 goog.addDependency('../../../animus/app.js', ['webgl.App'], []);
 goog.addDependency('../../../animus/asset.js', ['animus.Asset'], []);
-goog.addDependency('../../../animus/assetloader.js', ['animus.AssetLoader'], []);
-goog.addDependency('../../../animus/assetmanager.js', ['animus.AssetManager'], ['goog.structs.Map']);
-goog.addDependency('../../../animus/character.js', ['animus.Character'], []);
+goog.addDependency('../../../animus/assetloader.js', ['animus.AssetLoader'], ['animus.AssetManager', 'animus.Character', 'goog.structs.Map']);
+goog.addDependency('../../../animus/assetmanager.js', ['animus.AssetManager'], ['animus.Asset', 'goog.structs.Map']);
+goog.addDependency('../../../animus/character.js', ['animus.Character'], ['animus.Asset']);
 goog.addDependency('../../../animus/deps.js', [], []);
 goog.addDependency('../../../animus/program.js', ['webgl.Program'], []);
 goog.addDependency('../../../animus/renderer.js', ['webgl.Renderer'], []);
