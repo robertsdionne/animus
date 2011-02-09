8e1a2f102df79bdfae6e7eae65668526ce07f1ec
Robert Dionne <robertsdionne@gmail.com>
2011-02-08 22:17:49 -0500

    Remove genjsdeps. No longer using google closure library.

--- genjsdeps.sh
+++
@@ -1,6 +0,0 @@
-# Copyright 2011 Robert Scott Dionne. All Rights Reserved.
-#
-# Generates Javascript dependencies.
-../closure-library/closure/bin/build/depswriter.py \
-  --root_with_prefix=". ../../../animus/" \
-  --output_file=deps.js
