59e714735d8b7c1c7c7d7b0af69983f0880e0271
Robert Dionne <robertsdionne@gmail.com>
2011-03-02 23:35:13 -0800

    Add a man to the screen.

--- src/animus.js
+++ src/animus.js
@@ -32,8 +32,22 @@ animus.Renderer = function(keys) {
   /**
    * @type {WebGLBuffer}
    */
-  this.b_ = null;
+  this.arm_ = null;
 
+  /**
+   * @type {WebGLBuffer}
+   */
+  this.leg_ = null;
+
+  /**
+   * @type {WebGLBuffer}
+   */
+  this.torso_ = null;
+
+  /**
+   * @type {WebGLBuffer}
+   */
+  this.head_ = null;
 
   /**
    * @type {animus.Node}
@@ -78,7 +92,10 @@ animus.Renderer.prototype.onCreate = function(gl) {
   gl.enable(gl.DEPTH_TEST);
   gl.enable(gl.CULL_FACE);
 
-  this.b_ = gl.createBuffer();
+  this.arm_ = gl.createBuffer();
+  this.leg_ = gl.createBuffer();
+  this.torso_ = gl.createBuffer();
+  this.head_ = gl.createBuffer();
 
   this.p_.rotation =
       gl.getUniformLocation(this.p_.handle, 'rotation');
@@ -89,36 +106,81 @@ animus.Renderer.prototype.onCreate = function(gl) {
   this.p_.aNormal = gl.getAttribLocation(this.p_.handle, 'aNormal');
   this.p_.aColor = gl.getAttribLocation(this.p_.handle, 'aColor');
 
-  var a = new animus.BoxMan().add(0.2/N, 1/N, 0.2/N).build();
+  var a = new animus.BoxMan().add(0.2, 0.5, 0.2).build();
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, this.b_);
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.arm_);
   gl.bufferData(gl.ARRAY_BUFFER, a.byteLength, gl.STATIC_DRAW);
   gl.bufferSubData(gl.ARRAY_BUFFER, 0, a);
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  a = new animus.BoxMan().add(0.2, 1, 0.2).build();
 
-  var segment = new animus.Geometry(this.b_, this.p_);
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.leg_);
+  gl.bufferData(gl.ARRAY_BUFFER, a.byteLength, gl.STATIC_DRAW);
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, a);
+
+  a = new animus.BoxMan().add(1, 2, 0.2).build();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.torso_);
+  gl.bufferData(gl.ARRAY_BUFFER, a.byteLength, gl.STATIC_DRAW);
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, a);
+
+  a = new animus.BoxMan().add(0.5, 0.5, 0.5).build();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.head_);
+  gl.bufferData(gl.ARRAY_BUFFER, a.byteLength, gl.STATIC_DRAW);
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, a);
+
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+  var arm = new animus.Geometry(this.arm_, this.p_);
+  var leg = new animus.Geometry(this.leg_, this.p_);
+  var torso = new animus.Geometry(this.torso_, this.p_);
+  var head = new animus.Geometry(this.head_, this.p_);
+  var rightCalf = new animus.Transform(
+      animus.Quaternion.fromAxisAngle(animus.Vector.I, -Math.PI/4),
+      new animus.Vector(0, 1.1, 0));
+  rightCalf.children.push(leg);
+  var rightThigh = new animus.Transform(
+      animus.Quaternion.fromAxisAngle(animus.Vector.K, Math.PI).times(
+      animus.Quaternion.fromAxisAngle(animus.Vector.I, Math.PI/4)),
+      new animus.Vector(-0.4, -0.1, 0));
+  rightThigh.children.push(leg, rightCalf);
+  var leftCalf = new animus.Transform(
+      animus.Quaternion.fromAxisAngle(animus.Vector.I, -Math.PI/2),
+      new animus.Vector(0, 1.1, 0));
+  leftCalf.children.push(leg);
+  var leftThigh = new animus.Transform(
+      animus.Quaternion.fromAxisAngle(animus.Vector.K, -Math.PI).times(
+      animus.Quaternion.fromAxisAngle(animus.Vector.I, -Math.PI/6)),
+      new animus.Vector(0.4, -0.1, 0));
+  leftThigh.children.push(leg, leftCalf);
+  var rightForearm = new animus.Transform(
+      animus.Quaternion.fromAxisAngle(animus.Vector.I, Math.PI/3),
+      new animus.Vector(0, 0.6, 0));
+  rightForearm.children.push(arm);
+  var rightArm = new animus.Transform(
+      animus.Quaternion.fromAxisAngle(animus.Vector.I, Math.PI/3).times(
+      animus.Quaternion.fromAxisAngle(animus.Vector.K, 5*Math.PI/6)),
+      new animus.Vector(-0.6, 2, 0));
+  rightArm.children.push(arm, rightForearm);
+  var leftForearm = new animus.Transform(
+      animus.Quaternion.fromAxisAngle(animus.Vector.I, Math.PI/3),
+      new animus.Vector(0, 0.6, 0));
+  leftForearm.children.push(arm);
+  var leftArm = new animus.Transform(
+      animus.Quaternion.fromAxisAngle(animus.Vector.I, -Math.PI/3).times(
+      animus.Quaternion.fromAxisAngle(animus.Vector.K, -5*Math.PI/6)),
+      new animus.Vector(0.6, 2, 0));
+  leftArm.children.push(arm, leftForearm);
+  var skull = new animus.Transform(
+      null,
+      new animus.Vector(0, 2.1, 0));
+  skull.children.push(head);
   this.root_ = new animus.Transform();
-  this.root_.children.push(segment);
+  this.root_.children.push(
+      torso, rightThigh, leftThigh, rightArm, leftArm, skull);
   this.joint_ = [];
-  for (var i = 0; i < N-1; ++i) {
-    var joint = new animus.Transform();
-    joint.translation = new animus.Vector(0, 1/N, 0);
-    joint.rotation =
-        animus.Quaternion.fromAxisAngle(
-            animus.Vector.J, Math.PI/4/(i+1)).times(
-        animus.Quaternion.fromAxisAngle(
-            animus.Vector.K, Math.PI/N /* (i+1)*/));
-    joint.children.push(segment);
-    if (i == 0) {
-      this.root_.children.push(joint);
-    } else {
-      this.joint_[i-1].children.push(joint);
-    }
-    this.joint_[i] = joint;
-  }
-
-  this.root_.translation = new animus.Vector(0, -0.5, -2.0);
+  this.root_.translation = new animus.Vector(0, -0.5, -5.0);
 
   this.visitor_ = new animus.WebGlVisitor(gl);
 };
