c3392c865880731d5b8ad141d734ec5cc78cb7d0
Robert Dionne <robertsdionne@gmail.com>
2011-03-19 23:20:53 -0400

    Get the meat of a dual number down.

---
+++ src/dualnumber.js
@@ -0,0 +1,57 @@
+// Copyright 2011 Robert Scott Dionne. All Rights Reserved.
+
+/**
+ * @fileoverview Defines the dual number object.
+ * @author robertsdionne@gmail.com (Robert Scott Dionne)
+ */
+
+animus.DualNumber = function(real, dual) {
+  this.real = real;
+  this.dual = dual;
+};
+
+
+animus.DualNumber.prototype.conjugate = function() {
+  return new animus.DualNumber(this.real, -this.dual);
+};
+
+
+animus.DualNumber.prototype.plus = function(that) {
+  return new animus.DualNumber(this.real + that.real, this.dual + that.dual);
+};
+
+
+animus.DualNumber.prototype.minus = function(that) {
+  return new animus.DualNumber(this.real - that.real, this.dual - that.dual);
+};
+
+
+animus.DualNumber.prototype.negate = function() {
+  return new animus.DualNumber(-this.real, -this.dual);
+};
+
+
+animus.DualNumber.prototype.times = function(that) {
+  return new animus.DualNumber(
+      this.real * that.real,
+      this.real * that.dual + this.dual * that.real);
+};
+
+
+animus.DualNumber.prototype.inverse = function() {
+  return new animus.DualNumber(
+      1 / this.real,
+      -this.dual / this.real / this.real);
+};
+
+
+animus.DualNumber.prototype.over = function(that) {
+  return this.times(that.inverse());
+};
+
+
+animus.DualNumber.prototype.sqrt = function() {
+  return new animus.DualNumber(
+      Math.sqrt(this.real),
+      this.dual / 2 / Math.sqrt(this.real));
+};
