c2d6a727eba6421587e7d63c7c3cbf3edf6bcedb
Robert Dionne <robertsdionne@gmail.com>
2011-03-21 22:33:46 -0400

    Creating a joint palette.

---
+++ src/palette.js
@@ -0,0 +1,42 @@
+// Copyright 2011 Robert Scott Dionne. All Rights Reserved.
+
+/**
+ * @constructor
+ */
+animus.Palette = function() {
+  /**
+   * @type {Array.<animus.DualQuaternion>}
+   * @private
+   */
+  this.palette_ = [];
+};
+
+
+/**
+ * @param {number} i
+ * @param {animus.DualQuaternion} joint
+ */
+animus.Palette.prototype.set = function(i, joint) {
+  this.palette_[i] = joint;
+};
+
+
+/**
+ * @return {Array.<number>}
+ */
+animus.Palette.prototype.get = function() {
+  var result = [];
+  for (var i = 0; i < this.palette_.length; ++i) {
+    var joint = this.palette_[i];
+    result.push(
+        joint.vector.x.real,
+        joint.vector.y.real,
+        joint.vector.z.real,
+        joint.scalar.real,
+        joint.vector.x.dual,
+        joint.vector.y.dual,
+        joint.vector.z.dual,
+        joint.scalar.dual);
+  }
+  return result;
+};
