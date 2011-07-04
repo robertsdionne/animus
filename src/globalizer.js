1e23ec33d6029485f94f994636f1a87da6e21952
Robert Dionne <robertsdionne@gmail.com>
2011-07-03 21:12:31 -0700

    Adding a globalizer.

---
+++ src/globalizer.js
@@ -0,0 +1,22 @@
+// Copyright 2011 Robert Scott Dionne. All rights reserved.
+
+/**
+ */
+animus.Globalizer = function() {};
+
+
+/**
+ * @param {!animus.Pose}
+ */
+animus.Globalizer.prototype.globalize = function(pose) {
+  /** @type {animus.Pose} */
+  var result = {};
+  for (var thisBone in pose) {
+    result[thisBone] = pose[thisBone];
+    var thatBone = thisBone;
+    while (thatBone = pose[thatBone]) {
+      result[thisBone] = thatBone.times(result[thisBone]);
+    }
+  }
+  return result;
+};
