7d3d07b7e02d37e6d1d68d60ee1559bfadcbe8fd
Robert Dionne <robertsdionne@gmail.com>
2011-07-16 23:27:08 -0400

    Installed express for node.js.

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,11 @@
 // Copyright 2011 Robert Scott Dionne. All rights reserved.
 
+var express = require('express');
 var http = require('http');
 
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(8888);
-
-console.log('Server running at http://localhost:8888/');
+var app = express.createServer();
+app.get('/', function(request, response) {
+  response.redirect('http://www.google.com/');
+});
+app.listen(8888);
