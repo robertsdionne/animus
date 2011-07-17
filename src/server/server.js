b82318913432f3b5016d3743da085195968362e4
Robert Dionne <robertsdionne@gmail.com>
2011-07-16 23:04:38 -0400

    Adding a node.js server to serve as the basis of an animation editor.

---
+++ src/server/server.js
@@ -0,0 +1,9 @@
+// Copyright 2011 Robert Scott Dionne. All rights reserved.
+
+var http = require('http');
+
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen(8888);
